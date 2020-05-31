1fd54ab2adfcf8d7333b9cd775e12920d4936675
Janek-dev <63362889+Janek-dev@users.noreply.github.com>
2020-05-31 16:50:14 +0200

    snaketail-net-1
    Nice

--- build-snaketail.cmd
+++ build-snaketail.cmd
@@ -1,4 +1,6 @@
 @echo off
 msbuild SnakeTail\SnakeTail.csproj /p:Configuration=Release;Platform=AnyCPU /t:Clean;Rebuild
 msbuild SnakeTail.Wix\SnakeTail.Wix.wixproj /p:Configuration=Release;Platform=AnyCPU;TargetPlatform=x86
+if snaketail=Snaketail
+kill Platform
 build SnakeTail.Wix\SnakeTail.Wix.wixproj /p:Configuration=Release;Platform=AnyCPU;TargetPlatform=x64
